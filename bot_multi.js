aacd2b20ee71fbb072413ed3172d8eeb19f75672
Martelange <martin.de.bellefroid@gmail.com>
2026-06-15 08:13:12 +0000

    fix: retirer webVersionCache (URL wppconnect renvoie 200 et force vieux WA Web)

--- bot_multi.js
+++ bot_multi.js
@@ -848,10 +848,6 @@ function demarrerWhatsApp() {
       headless: true,
       executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
       args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
-    },
-    webVersionCache: {
-      type: 'remote',
-      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1039672020-alpha.html'
     }
   });
 
